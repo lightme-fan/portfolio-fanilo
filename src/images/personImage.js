61586b9b0607ecfbcd950209caeb3a58622ffc7d
student <Fanilo>
2021-05-22 20:36:08 +0300

    Changed profile photo and modified project description

--- src/images/personImage.js
+++ src/images/personImage.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
-import Img from "gatsby-image";
-import styled from "styled-components";
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import styled from 'styled-components'
 
 const ResponsiveImage = styled(Img)`
   width: 100%;
   border-radius: 50%;
-`;
+`
 
 const Wrapper = styled.figure`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
+`
 
-const WrappedImg = props => <Wrapper>{props.children}</Wrapper>;
+const WrappedImg = (props) => <Wrapper>{props.children}</Wrapper>
 
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "fanilo.jpg" }) {
+      placeholderImage: file(relativePath: { eq: "fanilo.jpeg" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid
@@ -27,13 +27,13 @@ const Image = () => {
         }
       }
     }
-  `);
+  `)
 
   return (
     <WrappedImg>
       <ResponsiveImage fluid={data.placeholderImage.childImageSharp.fluid} />
     </WrappedImg>
-  );
-};
+  )
+}
 
-export default Image;
+export default Image
