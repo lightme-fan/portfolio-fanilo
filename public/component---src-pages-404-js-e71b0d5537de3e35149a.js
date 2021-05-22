02ccd58724765b53581318a5713d53ede31318c0
student <Fanilo>
2021-05-23 00:25:06 +0300

    Added new Changes

---
+++ public/component---src-pages-404-js-e71b0d5537de3e35149a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{w2l6:function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return o}));var u=t("q1tI"),a=t.n(u);function o(){return a.a.createElement("div",null,a.a.createElement("h2",null,"Page Not Found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-e71b0d5537de3e35149a.js.map
