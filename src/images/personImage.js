ab68675aac7cbcf54f7f729d8f0e2eec46cf708b
student <Fanilo>
2021-06-05 22:20:17 +0300

    Added image shadow

--- src/images/personImage.js
+++ src/images/personImage.js
@@ -7,12 +7,16 @@ const ResponsiveImage = styled(Img)`
   width: 100%;
   border-radius: 50%;
   max-width: 210px;
+  box-shadow: 50px 23px 0px -1px rgba(14, 14, 97, 0.71);
+  -webkit-box-shadow: 50px 23px 0px -1px rgba(14, 14, 97, 0.71);
+  -moz-box-shadow: 50px 23px 0px -1px rgba(14, 14, 97, 0.71);
 `
 
 const Wrapper = styled.figure`
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
 `
 
 const WrappedImg = (props) => <Wrapper>{props.children}</Wrapper>
