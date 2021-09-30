7d94c7da13a3067e765706015a528c0208bcfcf7
lightme-fan <fanilo.and@onja.org>
2021-09-30 07:13:10 +0300

    Removed rest-country from the list of projects

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,14 +5,14 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/student/Coding/projects/portfolio-fanilo/.cache/dev-404-page.js"))),
-  "component---node-modules-gatsby-tech-blog-theme-src-components-search-js": hot(preferDefault(require("/home/student/Coding/projects/portfolio-fanilo/node_modules/gatsby-tech-blog-theme/src/components/Search.js"))),
-  "component---node-modules-gatsby-tech-blog-theme-src-templates-categories-query-js": hot(preferDefault(require("/home/student/Coding/projects/portfolio-fanilo/node_modules/gatsby-tech-blog-theme/src/templates/categories-query.js"))),
-  "component---node-modules-gatsby-tech-blog-theme-src-templates-category-js": hot(preferDefault(require("/home/student/Coding/projects/portfolio-fanilo/node_modules/gatsby-tech-blog-theme/src/templates/category.js"))),
-  "component---node-modules-gatsby-tech-blog-theme-src-templates-post-js": hot(preferDefault(require("/home/student/Coding/projects/portfolio-fanilo/node_modules/gatsby-tech-blog-theme/src/templates/post.js"))),
-  "component---node-modules-gatsby-tech-blog-theme-src-templates-posts-query-js": hot(preferDefault(require("/home/student/Coding/projects/portfolio-fanilo/node_modules/gatsby-tech-blog-theme/src/templates/posts-query.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/home/student/Coding/projects/portfolio-fanilo/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/home/student/Coding/projects/portfolio-fanilo/src/pages/index.js"))),
-  "component---src-pages-projects-js": hot(preferDefault(require("/home/student/Coding/projects/portfolio-fanilo/src/pages/projects.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/student/Coding/portfolio/portfolio-fanilo/.cache/dev-404-page.js"))),
+  "component---node-modules-gatsby-tech-blog-theme-src-components-search-js": hot(preferDefault(require("/home/student/Coding/portfolio/portfolio-fanilo/node_modules/gatsby-tech-blog-theme/src/components/Search.js"))),
+  "component---node-modules-gatsby-tech-blog-theme-src-templates-categories-query-js": hot(preferDefault(require("/home/student/Coding/portfolio/portfolio-fanilo/node_modules/gatsby-tech-blog-theme/src/templates/categories-query.js"))),
+  "component---node-modules-gatsby-tech-blog-theme-src-templates-category-js": hot(preferDefault(require("/home/student/Coding/portfolio/portfolio-fanilo/node_modules/gatsby-tech-blog-theme/src/templates/category.js"))),
+  "component---node-modules-gatsby-tech-blog-theme-src-templates-post-js": hot(preferDefault(require("/home/student/Coding/portfolio/portfolio-fanilo/node_modules/gatsby-tech-blog-theme/src/templates/post.js"))),
+  "component---node-modules-gatsby-tech-blog-theme-src-templates-posts-query-js": hot(preferDefault(require("/home/student/Coding/portfolio/portfolio-fanilo/node_modules/gatsby-tech-blog-theme/src/templates/posts-query.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/home/student/Coding/portfolio/portfolio-fanilo/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/home/student/Coding/portfolio/portfolio-fanilo/src/pages/index.js"))),
+  "component---src-pages-projects-js": hot(preferDefault(require("/home/student/Coding/portfolio/portfolio-fanilo/src/pages/projects.js")))
 }
 
