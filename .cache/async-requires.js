78ad7eb13b48d8837ddee4a702a42e8cb45b3d59
lightme-fan <fanilo.and@onja.org>
2021-12-01 09:37:21 +0300

    Renamed project github name

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,7 +2,7 @@
 const preferDefault = m => (m && m.default) || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": () => import("./../../caches/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---cache-caches-gatsby-plugin-offline-app-shell-js" */),
   "component---node-modules-gatsby-tech-blog-theme-src-components-search-js": () => import("./../../../node_modules/gatsby-tech-blog-theme/src/components/Search.js" /* webpackChunkName: "component---node-modules-gatsby-tech-blog-theme-src-components-search-js" */),
   "component---node-modules-gatsby-tech-blog-theme-src-templates-categories-query-js": () => import("./../../../node_modules/gatsby-tech-blog-theme/src/templates/categories-query.js" /* webpackChunkName: "component---node-modules-gatsby-tech-blog-theme-src-templates-categories-query-js" */),
   "component---node-modules-gatsby-tech-blog-theme-src-templates-category-js": () => import("./../../../node_modules/gatsby-tech-blog-theme/src/templates/category.js" /* webpackChunkName: "component---node-modules-gatsby-tech-blog-theme-src-templates-category-js" */),
